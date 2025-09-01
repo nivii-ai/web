de5d19ee97a936aef50e3acdd93f7bfa9446619d
Franco Sanchez <franco@nivii.ai>
2025-09-01 17:27:36 -0300

    fix: add scroll-smooth class to html element

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -80,9 +80,9 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className="scroll-smooth">
       <body
-        className={`${nunito.variable} antialiased scroll-smooth min-h-screen flex flex-col`}
+        className={`${nunito.variable} antialiased  min-h-screen flex flex-col`}
       >
         <NextIntlClientProvider messages={messages}>
           <Header locale={locale} />
